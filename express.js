52e0bc735a2aa31f98d48332e2cadd6cd2adfeae
Babita100 <babitasardar100@gmail.com>
2024-05-22 03:52:08 +0545

    express js

--- express.js
+++ express.js
@@ -2,12 +2,14 @@ const express= require('express');
 const app= express();
 
 app.get('',(req,res)=>{
-    res.send('this is home page');
+    res.send(`
+    this is home page <a href="/about">click here</a>    
+    `);   //link bhaira xaina
 });
 
 app.get('/about',(req,res)=>{
     res.send('this is about page');
 });
 
-app.listen(5000);
+app.listen(9000);
 //port already in use so error will arise
